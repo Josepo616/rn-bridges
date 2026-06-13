8e016b0e8767c27505f5d65ecd8b3b77df5f435b
José Álvarez <josemejia61976751@gmail.com>
2026-06-13 11:09:22 -0600

    feat: trigger haptic function added

--- src/NativeRnBridge.ts
+++ src/NativeRnBridge.ts
@@ -3,6 +3,7 @@ import { TurboModuleRegistry, type TurboModule } from 'react-native';
 export interface Spec extends TurboModule {
   multiply(a: number, b: number): number;
   greet(name: string): string;
+  triggerHaptic(type: string): void;
 }
 
 export default TurboModuleRegistry.getEnforcing<Spec>('RnBridge');
