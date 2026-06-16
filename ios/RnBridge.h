96a91477b05a3a83e9f7bc25a28880acf0a98467
José Álvarez <josemejia61976751@gmail.com>
2026-06-15 18:06:36 -0600

    feat: inherit from SpecBase and conform to network delegate

--- ios/RnBridge.h
+++ ios/RnBridge.h
@@ -1,5 +1,9 @@
 #import <RnBridgeSpec/RnBridgeSpec.h>
+#import "RnBridge-Swift.h"
 
-@interface RnBridge : NSObject <NativeRnBridgeSpec>
+NS_ASSUME_NONNULL_BEGIN
 
+@interface RnBridge : NativeRnBridgeSpecBase <NativeRnBridgeSpec, RnBridgeNetworkDelegate>
 @end
+
+NS_ASSUME_NONNULL_END
